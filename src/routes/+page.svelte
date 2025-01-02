395c30ce1253b95b0f492b415d95296b1153a6f1
romiyusnandar <yusromi04@gmail.com>
2025-01-02 15:27:04 +0700

    Add simple homepage ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="p-5 mb-4 bg-light rounded-3 shadow-sm">
+  <div class="container-fluid py-5">
+    <h1 class="display-5 fw-bold">Belajar dasar svelte</h1>
+    <p class="col-md-12 fs-4">Fullstack Javascript menggunakan Express dan Svelte</p>
+    <hr />
+    <a href="/register" class="btn btn-primary btn-lg me-3">REGISTER</a>
+    <a href="/login" class="btn btn-secondary btn-lg">LOGIN</a>
+  </div>
+</div>
